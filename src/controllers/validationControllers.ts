f0a4c20e8696eee66728e25460f179b46b417016
extedcouD <extedcoud@gmail.com>
2025-01-13 15:17:46 +0530

    changes

--- src/controllers/validationControllers.ts
+++ src/controllers/validationControllers.ts
@@ -1,12 +1,38 @@
 import { NextFunction, Request, Response } from "express";
 import logger from "../utils/logger";
+
 import { ApiRequest } from "../routes/manual";
+import { performL2Validations } from "../generated/L2-validations";
+import { loadSessionData } from "../services/data-services";
 
 export async function l2Validation(
 	req: ApiRequest,
 	res: Response,
 	next: NextFunction
 ) {
-	logger.debug("l2 validations not implemented yet");
-	next();
+	try {
+		const sessionData = await loadSessionData(req.body.context.transaction_id);
+		const errors = performL2Validations(
+			req.params.action,
+			req.body,
+			false,
+			sessionData
+		);
+		const firstError = errors.find((s) => !s.valid);
+		if (firstError) {
+			req.l2Error = {
+				code: firstError.code,
+				message: firstError.description || "validation failed",
+			};
+		}
+		logger.info(
+			`L2 validations completed found ${
+				errors.filter((s) => !s.valid).length
+			} errors`
+		);
+		next();
+	} catch (e) {
+		logger.error("failed to run L2 validations", e);
+		next();
+	}
 }
