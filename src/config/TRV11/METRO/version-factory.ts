150335e035a201f4e72a9be533fecca1ed0241a6
Shivang Dudani <shivangdudani113@gmail.com>
2025-02-10 16:32:56 +0530

    minor change

--- src/config/TRV11/METRO/version-factory.ts
+++ src/config/TRV11/METRO/version-factory.ts
@@ -3,6 +3,7 @@ import { createMockReponse2 } from "./2.0.1/generation-pipline";
 import { createMockReponse1 } from "./2.0.0/generation-pipline";
  
 export async function createMockResponse(version: string,sessionData: SessionData,action_id: string){
+    console.log("The version is", version)
     if(version === "2.0.0"){
         return createMockReponse1(action_id,sessionData)
     } else if(version === "2.0.1"){
