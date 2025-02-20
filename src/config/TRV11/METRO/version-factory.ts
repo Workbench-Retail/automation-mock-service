d9092bd75e10a58587a854149a093d4fcb031906
Shivang Dudani <shivangdudani113@gmail.com>
2025-02-20 11:32:37 +0530

    using usedb 0

--- src/config/TRV11/METRO/version-factory.ts
+++ src/config/TRV11/METRO/version-factory.ts
@@ -3,22 +3,29 @@ import { createMockResponseMETRO201 } from "./2.0.1/generation-pipline";
 import { createMockResponseMETRO200 } from "./2.0.0/generation-pipline";
 import { ApiServiceCache, RedisService } from "ondc-automation-cache-lib";
 import { SessionCache } from "../../../types/api-session-cache";
-import { createMockReponseBUS200} from "../BUS/2.0.0/generation-pipline";
-export async function createMockResponse(session_id: string,sessionData: SessionData,action_id: string){
-    const apiCache = new ApiServiceCache(RedisService);
-    const api_session = await apiCache.getSessionIdFromAPIService(session_id) ?? "";
-    const data = JSON.parse(api_session) as SessionCache;
-    const {version,usecaseId} = data;
-    if(usecaseId === "METRO"){
-        if(version === "2.0.0"){
-            return createMockResponseMETRO200(action_id,sessionData)
-        } else if(version === "2.0.1"){
-            return createMockResponseMETRO201(action_id,sessionData)
-        }
-    } else if(usecaseId === "BUS"){
-        if(version === "2.0.0"){
-            return createMockReponseBUS200(action_id,sessionData)
-        }
+import { createMockReponseBUS200 } from "../BUS/2.0.0/generation-pipline";
+export async function createMockResponse(
+  session_id: string,
+  sessionData: SessionData,
+  action_id: string
+) {
+  const apiCache = new ApiServiceCache(RedisService);
+  RedisService.useDb(0)
+  const api_session =
+    (await apiCache.getSessionIdFromAPIService(session_id)) ?? "";
+    console.log("api_session is ",api_session)
+  const data = JSON.parse(api_session) as SessionCache;
+  console.log("data is",data)
+  const { version, usecaseId } = data;
+  if (usecaseId === "METRO") {
+    if (version === "2.0.0") {
+      return createMockResponseMETRO200(action_id, sessionData);
+    } else if (version === "2.0.1") {
+      return createMockResponseMETRO201(action_id, sessionData);
     }
-
+  } else if (usecaseId === "BUS") {
+    if (version === "2.0.0") {
+      return createMockReponseBUS200(action_id, sessionData);
+    }
+  }
 }
