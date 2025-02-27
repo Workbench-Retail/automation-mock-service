61a059932387f7e1a16dc228e850c0de370e27b3
Shivang Dudani <shivangdudani113@gmail.com>
2025-02-27 12:34:09 +0530

    testing

--- src/config/TRV11/BUS/2.0.0/on_select/generator.ts
+++ src/config/TRV11/BUS/2.0.0/on_select/generator.ts
@@ -126,10 +126,9 @@ export async function onSelectGenerator(
                 count: ids_with_quantities["items"][item.id] ?? 0, // Default to 0 if not in the mapping
             },
         },
-    }))
-    .filter((item) => item.quantity.selected.count > 0);
-    items = updatedItems;
-    console.log(items)
+    })).filter((item) => item.quantity.selected.count > 0);
+
+    console.log("updated_items",updatedItems)
     createAndAppendFulfillments(updatedItems, fulfillments);
     const quote = createQuoteFromItems(updatedItems);
     existingPayload.message.order.items = items;
