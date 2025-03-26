3166a5e534241e681c27e57856bf4d704afaa0f0
ankit-wits <80715550+ankit-apk@users.noreply.github.com>
2025-03-26 13:44:22 +0530

    add generators

--- src/config/TRV10/2.1.0/confirm/generator_multiple_stops.ts
+++ src/config/TRV10/2.1.0/confirm/generator_multiple_stops.ts
@@ -10,11 +10,11 @@ const customer = {
 }
 
 export async function confirmMultipleStopsGenerator(existingPayload: any,sessionData: SessionData){
-    existingPayload.order.fulfillments = sessionData.fulfillments
-    existingPayload.order.fulfillments[0]["customer"] = customer
+    existingPayload.message.order.fulfillments = sessionData.fulfillments
+    existingPayload.message.order.fulfillments[0]["customer"] = customer
     existingPayload.order.items[0] = {
         id : sessionData.selected_item_id 
     }
-    existingPayload.order.payments = sessionData.payments
+    existingPayload.message.order.payments = sessionData.payments
     return existingPayload;
 }
---
+++ src/config/TRV10/2.1.0/on_confirm/on_confirm_driver_assigned/generator_multiple_stops.ts
@@ -0,0 +1,70 @@
+import { randomBytes } from "crypto";
+import { SessionData } from "../../../session-types";
+
+const agent  = {
+            "contact": {
+                "phone": "9856798567"
+            },
+            "person": {
+                "name": "Jason Roy"
+            }
+        }
+function updateFulfillments(fulfillments: any[]) {
+    return fulfillments.map(fulfillment => {
+        // Add the vehicle object to each fulfillment
+        fulfillment.vehicle = {
+        category: "AUTO_RICKSHAW",
+        variant: "AUTO_RICKSHAW",
+        make: "Bajaj",
+        model: "Compact RE",
+        registration: "KA-01-AD-9876"
+        };
+    
+        // Find the stop with type "START"
+        const startStop = fulfillment.stops.find((stop:any) => stop.type === "START");
+    
+        // If found, add the authorization object
+        if (startStop) {
+        startStop.authorization = {
+            token: "234234",
+            type: "OTP",
+            valid_to: "2023-12-10T08:05:34.294Z",
+            status: "UNCLAIMED"
+        };
+        }
+    
+        return fulfillment;
+    });
+    }
+
+export async function onConfirmMultipleStopsGenerator(
+    existingPayload: any,
+    sessionData: SessionData
+) {
+    const randomId = Math.random().toString(36).substring(2, 15);
+    const order_id = randomId;
+    existingPayload.message.order.payments = sessionData.payments
+    
+      // Check if items is a non-empty array
+    if (sessionData.items.length > 0) {
+    existingPayload.message.order.items = sessionData.items;
+
+    }
+
+    // Check if fulfillments is a non-empty array
+    if (sessionData.fulfillments.length > 0) {
+    existingPayload.message.order.fulfillments = sessionData.fulfillments;
+    existingPayload.message.order.fulfillments = updateFulfillments(existingPayload.message.order.fulfillments)
+    existingPayload.message.order.fulfillments[0]["agent"] = agent
+    }
+    if(sessionData.quote != null){
+    existingPayload.message.order.quote = sessionData.quote
+    }
+    if(sessionData.items.length > 0){
+        existingPayload.message.order.items = sessionData.items
+    }
+    existingPayload.message.order.id = order_id;
+    existingPayload.message.order.status = "ACTIVE"
+    existingPayload.message.order.payments = sessionData.payments;
+    return existingPayload;
+}
