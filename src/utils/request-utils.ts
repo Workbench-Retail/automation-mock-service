6a260740d62454003f2b41445a84c37fb283f226
Shivang Dudani <shivangdudani113@gmail.com>
2025-05-29 13:05:32 +0530

    changing API_SERVICE_LAYER to API_SERVICE_URL

--- src/utils/request-utils.ts
+++ src/utils/request-utils.ts
@@ -1,72 +1,33 @@
 import axios from "axios";
-import { logger, logInfo } from "./logger";
 import { saveData } from "../services/data-services";
-function delay(ms: number): Promise<void> {
-	return new Promise((resolve) => setTimeout(resolve, ms));
-}
+import { logger } from "./logger";
+
 export async function sendToApiService(
 	action: string,
 	body: any,
-	queryData: any
+	queryData = {}
 ) {
-	logInfo({
-		message: "Entering sendToApiService",
-		meta: { action, body, queryData },
-		transaction_id: body.context.transaction_id,
-	});
 	try {
 		const domain = process.env.DOMAIN;
 		const version = body.context.version ?? body.context.core_version;
-		const url = `${process.env.API_SERVICE_LAYER}/${domain}/${version}/mock/${action}`;
-		const subscriber_url = queryData.subscriber_url;
-		if (!subscriber_url) {
-			//   logger.error("subscriber url not provided");
-			logInfo({
-				message: "Exiting sendToApiService. Subscriber URL not provided",
-				meta: { action, body, queryData },
-				transaction_id: body.context.transaction_id,
-			});
-			throw new Error("subscriber url not provided ");
-		}
+		const url = `${process.env.API_SERVICE_URL}/${domain}/${version}/mock/${action}`;
+		console.log(action, JSON.stringify(body.message, null, 2));
 		await saveData(action, body);
-		// logger.debug(`Sending response to api service ${url} ${action}`);
-		logInfo({
-			message: "Sending response to api service",
-			meta: { action, body, queryData },
-			transaction_id: body.context.transaction_id,
-		});
+		logger.debug(`Sending response to api service ${url} ${action}`);
 		await axios.post(url, body, {
 			params: {
 				...queryData,
 			},
 		});
-		logInfo({
-			message: "Exiting sendToApiService",
-			meta: { action, body, queryData },
-			transaction_id: body.context.transaction_id,
-		});
 	} catch (err) {
-		// logger.error("Error in sending response to api service", err);
-		logInfo({
-			message: "Error in sending response to api service",
-			meta: { action, body, queryData },
-			transaction_id: body.context.transaction_id,
-		});
+		logger.error("Error in sending response to api service", err);
 	}
 }
 
 export function createSellerUrl(domain: string, version: string) {
-	logInfo({
-		message: "Inside createSellerUrl",
-		meta: { domain, version },
-	});
-	return `${process.env.API_SERVICE_LAYER}/${domain}/${version}/seller`;
+	return `${process.env.API_SERVICE_URL}/${domain}/${version}/seller`;
 }
 
 export function createBuyerUrl(domain: string, version: string) {
-	logInfo({
-		message: "Inside createBuyerUrl",
-		meta: { domain, version },
-	});
-	return `${process.env.API_SERVICE_LAYER}/${domain}/${version}/buyer`;
+	return `${process.env.API_SERVICE_URL}/${domain}/${version}/buyer`;
 }
