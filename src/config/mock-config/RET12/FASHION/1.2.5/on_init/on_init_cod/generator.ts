5726d5e21ac7928e525592541747e7114bd7d75b
Prabhkirat Singh <prabhkirat.singh@thewitslab.com>
2025-06-11 05:10:36 +0530

    added cod

---
+++ src/config/mock-config/RET12/FASHION/1.2.5/on_init/on_init_cod/generator.ts
@@ -0,0 +1,23 @@
+import { SessionData } from "../../../../session-types";
+import { getUpdatedBilling } from "../../api-objects/billing";
+import { createFulfillments } from "../../api-objects/fulfillments";
+
+export async function on_init_cod_generator(
+	existingPayload: any,
+	sessionData: SessionData
+) {
+	console.log("###### on init_generator ####");
+	existingPayload.message.order.items = sessionData.items;
+	existingPayload.message.order.fulfillments = createFulfillments(
+		"on_init",
+		"on_init",
+		sessionData,
+		existingPayload.message.order.fulfillments
+	);
+	existingPayload.message.order.billing = getUpdatedBilling(
+		sessionData.billing
+	);
+	existingPayload.message.order.provider = sessionData.provider;
+	existingPayload.message.order.quote = sessionData.quote;
+	return existingPayload;
+}
