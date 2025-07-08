2be9a85b247868ebbdca30c324ed76e71cf62e68
extedcouD <extedcoud@gmail.com>
2025-07-08 12:37:56 +0530

    delay

--- src/config/mock-config/RET10/GROCERY/1.2.5/cancel/cancel_force/generator.ts
+++ src/config/mock-config/RET10/GROCERY/1.2.5/cancel/cancel_force/generator.ts
@@ -4,14 +4,7 @@ export async function cancel_force_generator(
 	existingPayload: any,
 	sessionData: SessionData
 ) {
-	await delay(5000);
+	// await delay(5000);
 	existingPayload.message.order_id = sessionData.order_id;
 	return existingPayload;
-}
-
-/**
- * Returns a Promise that resolves after a given delay in milliseconds.
- */
-export function delay(ms: number): Promise<void> {
-	return new Promise((resolve) => setTimeout(resolve, ms));
 }
--- src/config/mock-config/RET10/GROCERY/1.2.5/generator.ts
+++ src/config/mock-config/RET10/GROCERY/1.2.5/generator.ts
@@ -111,7 +111,7 @@ import { on_status_self_pickup_picked_generator } from "./on_status/on_status_se
 import { cancel_force_generator } from "./cancel/cancel_force/generator";
 import { on_cancel_force_generator } from "./on_cancel/on_cancel_force/generator";
 import { cancel_breach_generator } from "./cancel/cancel_breach/generator";
-import e from "express";
+import { delay } from "../../../../../utils/generic-utils";
 
 export async function Generator(
 	action_id: string,
@@ -402,6 +402,7 @@ export async function Generator(
 		case "search_inc_stop":
 			return search_inc_generator(existingPayload, sessionData);
 		case "cancel_force":
+			await delay(5000);
 			return cancel_force_generator(existingPayload, sessionData);
 		case "on_cancel_force":
 			return on_cancel_force_generator(existingPayload, sessionData);
